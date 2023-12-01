a0d01edd01eb49c8b5bf6cbedaf6fa406ffafc9c
Sanjeev-gour <sanjeevgour2004@gmail.com>
2023-12-01 14:29:14 +0530

    song class

---
+++ practicals/song.php
@@ -0,0 +1,93 @@
+<?php
+
+/**
+ * Class Song
+ * Represents a musical song with properties like title, artist, genre, and tempo.
+ */
+class Song
+{
+    private $title;
+    private $artist;
+    private $genre;
+    private $tempo;
+
+    /**
+     * Get the title of the song.
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set the title of the song.
+     *
+     * @param string $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * Get the artist of the song.
+     *
+     * @return string
+     */
+    public function getArtist()
+    {
+        return $this->artist;
+    }
+
+    /**
+     * Set the artist of the song.
+     *
+     * @param string $artist
+     */
+    public function setArtist($artist)
+    {
+        $this->artist = $artist;
+    }
+
+    /**
+     * Get the genre of the song.
+     *
+     * @return string
+     */
+    public function getGenre()
+    {
+        return $this->genre;
+    }
+
+    /**
+     * Set the genre of the song.
+     *
+     * @param string $genre
+     */
+    public function setGenre($genre)
+    {
+        $this->genre = $genre;
+    }
+
+    /**
+     * Get the tempo of the song.
+     *
+     * @return int
+     */
+    public function getTempo()
+    {
+        return $this->tempo;
+    }
+
+    /**
+     * Set the tempo of the song.
+     *
+     * @param int $tempo
+     */
+    public function setTempo($tempo)
+    {
+        $this->tempo = $tempo;
+    }
+}
