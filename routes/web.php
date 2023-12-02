98d754193f5f52634b9e5534f00f8d0764f49ffd
Sanjeev-gour <sanjeevgour2004@gmail.com>
2023-12-02 12:17:40 +0530

    Test Cases Practical and Baigan test passes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,30 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/veggies', function () {
+    return view('veggies');
+});
+Route::get('/veggies/{veggieName}', function (string $veggieName){
+	return $veggieName;
+})->whereIn('veggieName',['baigan','bhindi','aaloo','gobhi']);
+Route::get('/songs', function () {
+    $song = new Song();
+    $song->setTitle('With You');
+    return view('songs', [ 'song' => $song ]);
+});
+Route::get('/songs', function () {
+    $song1 = new Song();
+    $song1->setTitle("Stan");
+    $song1->setArtist("Eminem");
+  
+    $song2 = new Song();
+    $song2->setTitle("Nothing Else Matters");
+    $song2->setArtist("Metallica");
+  
+    $song3 = new Song();
+    $song3->setTitle("With You");
+    $song3->setArtist("A P Dhillon");
+  
+    return view('songs', [ 'songs' => [ $song1, $song2, $song3 ] ]); 
+  });
--- tests/Unit/PracticalTest.php
+++ tests/Unit/PracticalTest.php
@@ -2,20 +2,84 @@
 
 namespace Tests\Unit;
 
+use InvalidArgumentException;
 use PHPUnit\Framework\TestCase;
 
-require(__DIR__.'/../../practicals/Practicals.php');
-use practicals\Practical;
+require(__DIR__.'/../../practicals/Practical.php');
+use Practicals\Practical;
 
 class PracticalTest extends TestCase
 {
     /**
-     * A basic unit test example.
+     * A basic unit test for add
      */
     public function testAdd(): void
     {
         $this->assertEquals(Practical::add(1,3), 4);
     }
+    public function testAddAcceptsFloat(): void
+    {
+        $this->assertEquals(Practical::add(1.5,3.5),5.0);
+    }
+
+    public function testAddAcceptsFloatInteger(): void
+    {
+        $this->assertEquals(Practical::add(1.5,2),3.5);
+    }
+
+    public function testAddAcceptsIntegerString(): void
+    {
+        $this->assertEquals(Practical::add("1","2"),3);
+    }
+    public function testAddAcceptsFloatString(): void
+    {
+        $this->assertEquals(Practical::add("1.4","3.14"),4.54);
+    }
+
+    public function testAddRejectsAlphabetString(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        Practical::add("abc","xyz");
+    }    
+
+    public function testAddRejectsAlphanumericString(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        Practical::add("a1",1);
+    }
+
+    /**
+     * Unit test for generateFibonacciSequence
+     */
+    public function testFibonacciAcceptsInteger()
+    {
+        $practical = new Practical();
+        $result = $practical->generateFibonacciSequence(4);
+        $this->assertEquals([0, 1, 1, 2], $result);
+    }
+
+    public function testFibonacciRejectsIntegerString()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $practical = new Practical();
+        $practical->generateFibonacciSequence("4");
+    }
+    
+    public function testFibonacciRejectsAlphabetString(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        Practical::generateFibonacciSequence("abc");
+    }
+
+    public function testFibonacciRejectsFloat()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $practical = new Practical();
+        $practical->generateFibonacciSequence(3.5);
+    }
+
+    
+
 }
 
 ?>
